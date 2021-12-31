928809e86cc975427839b8ba62873fc70f04058a
zhouyijun <zhou495248579@hotmail.com>
2021-12-31 22:38:15 +0800

    base config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,29 +3,14 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint",
-  ],
+  extends: ["plugin:vue/vue3-essential", "@vue/standard"],
   parserOptions: {
-    ecmaVersion: 2020,
+    parser: "babel-eslint",
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    indent: 0,
+    "space-before-function-paren": 0,
   },
-  overrides: [
-    {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
-      ],
-      env: {
-        jest: true,
-      },
-    },
-  ],
 };
