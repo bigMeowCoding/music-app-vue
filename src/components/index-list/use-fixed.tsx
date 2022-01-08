9a96ebe14e1a7f40a818c52e4905821bd6e1c6c4
zhouyijun <zhou495248579@hotmail.com>
2022-01-08 19:15:08 +0800

    头部导航滚动上移动画

--- src/components/index-list/use-fixed.tsx
+++ src/components/index-list/use-fixed.tsx
@@ -2,15 +2,29 @@ import { ref, watch, computed, nextTick } from "vue";
 
 export default function useFixed(props: any) {
   const groupRef = ref<HTMLElement | null>(null);
+  const TITLE_HEIGHT = 30;
+
   const scrollY = ref(0);
   const currentIndex = ref(0);
   const listHeights = ref<number[]>([]);
+  const distance = ref(0);
+
   const fixedTitle = computed(() => {
     if (scrollY.value < 0) {
       return "";
     }
     const currentGroup = props.data[currentIndex.value];
     return currentGroup ? currentGroup.title : "";
+  });
+  const fixedStyle = computed(() => {
+    const distanceVal = distance.value;
+    let diff = 0;
+    if (distanceVal > 0 && distanceVal < TITLE_HEIGHT) {
+      diff = distanceVal - TITLE_HEIGHT;
+    }
+    return {
+      transform: `translate3d(0,${diff}px,0)`,
+    };
   });
   watch(
     () => {
@@ -40,6 +54,7 @@ export default function useFixed(props: any) {
       const heightBottom = listHeightVal[i + 1];
       if (newY >= heightTop && newY < heightBottom) {
         currentIndex.value = i;
+        distance.value = heightBottom - newY;
         break;
       }
     }
@@ -50,6 +65,7 @@ export default function useFixed(props: any) {
   return {
     groupRef,
     onScroll,
+    fixedStyle,
     fixedTitle,
     currentIndex,
   };
